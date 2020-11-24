a17e54edbb1f0b75faf43b0754979ca6708b72de
Sharanya01 <73655236+Sharanya01@users.noreply.github.com>
2020-11-24 09:45:18 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,26 @@
+class Box{
+  constructor(x,y,w,h){
+    var box_options = {
+
+ isStatic : true
+    }  
+
+    this.body = Bodies.rectangle(x,y,w,h,box_options);
+    this.width = w;
+    this.height = h;
+        World.add(world,this.body)
+
+  }
+  display(){
+    var pos = this.body.position;
+    var angle=  this.body.angle;
+   push();
+   translate(pos.x,pos.y)
+   rotate(angle);
+       rectMode(CENTER);
+fill("turquoise")
+
+rect(0,0,this.width ,this.height);
+pop();
+  }
+} 
